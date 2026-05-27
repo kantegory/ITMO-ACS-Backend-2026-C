e44ac25c754ed54be48403631a74a93c5dbfd90f
popovnikitaserg <popovnikita.serg@gmail.com>
2026-05-27 13:39:23 +0300

    test deploy

--- БР2.2/Попов Никита/labs/lab4/services/bookings-service/src/middlewares/auth.middleware.ts
+++ БР2.2/Попов Никита/labs/lab4/services/bookings-service/src/middlewares/auth.middleware.ts
@@ -3,7 +3,7 @@ import { config } from "../config";
 
 export function internalAuthMiddleware(req: Request, res: Response, next: NextFunction) {
   if (req.header("X-Internal-Api-Key") !== config.internalApiKey) {
-    return res.status(401).json({ code: "UNAUTHORIZED", message: "Требуется аутентификация внутреннего вызова" });
+    return res.status(401).json({ code: "UNAUTHORIZED", message: "Требуется аутентификация внутреннего вызова!" });
   }
   next();
 }
